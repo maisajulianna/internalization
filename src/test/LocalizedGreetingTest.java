4ad8840fd02ea530cd35670c15ec31d95b08c83e
maisajulianna <maisa.osterman@gmail.com>
2024-10-24 10:39:30 +0300

    Initial commit

---
+++ src/test/LocalizedGreetingTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import static org.junit.Assert.assertEquals;
+
+public class LocalizedGreetingTest {
+
+    @Test
+    public void testEnglishGreeting() {
+        Locale locale = new Locale("en", "US");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("hello", greeting);
+    }
+
+    @Test
+    public void testSpanishGreeting() {
+        Locale locale = new Locale("es", "ES");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("hola", greeting);
+    }
+
+    @Test
+    public void testFrenchGreeting() {
+        Locale locale = new Locale("fr", "FR");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("bonjour", greeting);
+    }
+
+    @Test
+    public void testInvalidChoiceDefaultsToEnglish() {
+        Locale locale = new Locale("en", "US"); // Simulate default choice
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("hello", greeting); // Expected default is English
+    }
+}
+
